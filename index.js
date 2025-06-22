d92665c63afc2b08d64d4a6561902afb894c5be9
shiyyaas <shiyasofficial1@gmail.com>
2025-06-22 20:46:45 +0530

    Finally figured to change the button when clicking

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ feather.replace(); //For CDN Icons
 
 // Theme changing
 function Lightmode() {
+    console.log(5-4);
+    // Icon changing
+    icon = document.getElementsByClassName('cdnicon')[0];
+    icon.setAttribute('data-feather','moon');
+    feather.replace();
+
+    // x = document.getElementsByTagName('body')[0];
+    // x.style.backgroundColor = "white";
+    
     console.log("shiyas");
 }
