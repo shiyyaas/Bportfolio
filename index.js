2329df37ee881600e4156b683156928a4c24c675
shiyyaas <shiyasofficial1@gmail.com>
2025-06-22 21:03:57 +0530

    Darkmode done with crtl c && crtl v

--- index.js
+++ index.js
@@ -12,11 +12,19 @@ function Lightmode() {
     icon.style.color = "white";
     feather.replace(); // For feather icons , you should refresh like this
 
-    
-
 
     // x = document.getElementsByTagName('body')[0];
     // x.style.backgroundColor = "white";
     
-    console.log("shiyas");
+}
+function Darkmode() {
+    darkmode = document.getElementsByClassName('floatingbtn2')[0];
+    darkmode.setAttribute('onclick','Lightmode()')
+    // Button
+    btn2 = document.getElementsByClassName('floatingbtn2')[0];
+    btn2.style.backgroundColor = "white"; 
+    icon = document.getElementsByClassName('cdnicon')[0];
+    icon.setAttribute('data-feather','sun');
+    icon.style.color = "black";
+    feather.replace(); // For feather icons , you should refresh like this
 }
